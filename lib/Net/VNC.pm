63886e29e538a7b63a2690d901d75c8ef8065d41
Leon Brocard <acme@astray.com>
2009-11-23 14:41:58 +0000

    Fix problem with passwords over 8 characters (spotted by binox)

--- lib/Net/VNC.pm
+++ lib/Net/VNC.pm
@@ -265,7 +265,7 @@ sub _handshake_security {
 
         #    warn "chal: " . unpack('h*', $challenge) . "\n";
 
-        my $key = $self->password;
+        my $key = substr( $self->password, 0, 8 );
         $key = '' if ( !defined $key );
         $key .= pack( 'C', 0 ) until ( length($key) % 8 ) == 0;
 
